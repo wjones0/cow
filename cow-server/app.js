b4122e323a760c76912b5c765f75e09c367015fd
William Jones <wjones0@users.noreply.github.com>
2016-05-01 13:26:04 -0400

    set up for jwt authorization

--- cow-server/app.js
+++ cow-server/app.js
@@ -4,15 +4,29 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var jwt = require('express-jwt');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var sets = require('./api/sets');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// setup cors
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// jwt setup
+var jwtCheck = jwt({
+  secret: new Buffer(process.env.JWT_CLIENT_SECRET, 'base64'),
+  audience: process.env.JWT_CLIENT_ID
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +37,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/sets', jwtCheck);
+app.use('/api/sets', sets);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
