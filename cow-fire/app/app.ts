c903aac575561068ed5d4ff413bdcaef7c09333e
William Jones <wjones0@users.noreply.github.com>
2016-05-30 14:03:31 -0400

    initial commit of new mobile with firebase

---
+++ cow-fire/app/app.ts
@@ -0,0 +1,33 @@
+import {App, Platform} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {LoginPage} from './pages/login/login';
+
+import {
+    FIREBASE_PROVIDERS, defaultFirebase,
+    AngularFire, firebaseAuthConfig, AuthProviders,
+    AuthMethods
+} from 'angularfire2';
+
+@App({
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  providers: [
+        FIREBASE_PROVIDERS,
+        defaultFirebase('https://cowfire.firebaseio.com/'),
+        firebaseAuthConfig({
+          provider: AuthProviders.Google,
+          method: AuthMethods.Popup
+        })
+  ],
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+})
+export class MyApp {
+  rootPage: any = LoginPage;
+
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+}
---
+++ cow-fire/app/pages/login/login.ts
@@ -0,0 +1,23 @@
+import {Page, NavController} from 'ionic-angular';
+import {AngularFire} from 'angularfire2';
+
+
+/*
+  Generated class for the LoginPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Page({
+  templateUrl: 'build/pages/login/login.html',
+})
+export class LoginPage {
+  
+
+  constructor(private _nav: NavController, private _af: AngularFire) {
+  }
+  
+  login() {
+    this._af.auth.login();
+  }
+}
